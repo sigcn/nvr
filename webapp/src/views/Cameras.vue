7e5a1d5b6a87ae642a3d463f730c5a89d9095214
rkonfj <rkonfj@gmail.com>
2024-10-22 19:47:34 +0800

    webapp/cameras: add camera description

--- webapp/src/views/Cameras.vue
+++ webapp/src/views/Cameras.vue
@@ -26,14 +26,18 @@ onMounted(async () => {
           cameras.value[i].fps = mediaInfo.fps
       });
       v.player = player
+      cameras.value[i].loading = true
       player.load()
       try {
         await player.play()
         cameras.value[i].playing = true
+        cameras.value[i].loading = false
       } catch (_) {
         setTimeout(() => {
           player.unload()
           player.detachMediaElement()
+          cameras.value[i].playing = false
+          cameras.value[i].loading = false
         }, 100)
       }
     }
@@ -55,7 +59,7 @@ async function loadCameras(session) {
   cameras.value = r.data
 }
 
-function play(i) {
+async function play(i) {
   if (cameras.value[i].playing) {
     stop(i)
     return
@@ -63,9 +67,10 @@ function play(i) {
   let player = videos.value[i].player
   player.attachMediaElement(videos.value[i])
   player.load()
-  player.play()
+  cameras.value[i].loading = true
+  await player.play()
   cameras.value[i].playing = true
-
+  cameras.value[i].loading = false
 }
 
 function stop(i) {
@@ -78,14 +83,19 @@ function stop(i) {
 <template>
   <ul v-for="(cam, i) in cameras">
     <li>
+      <div class="bg" v-if="!cam.playing">
+      no video signal
+      </div>
       <video
         ref="videos"
         class="video"
+        @mouseenter="cam.showmenu=true"
         :data-url="`/media/${cam.id}/live.ts`"
+        :style="`display: ${cam.playing?'block':'none'}`"
       ></video>
-      <div class="menu">
+      <div class="menu" v-if="!cam.playing || cam.showmenu" @mouseleave="cam.showmenu=false">
         <div class="play" @click="play(i)">
-          {{ cam.playing ? 'Stop' : 'Play' }}
+          {{ cam.playing ? 'Stop' : cam.loading ? '•' : 'Play' }}
         </div>
         <div class="media">
           <label>videoCodec</label>
@@ -95,6 +105,14 @@ function stop(i) {
           <label>fps</label>
           <span>{{ cam.fps }}</span>
         </div>
+      </div>
+      <div class="desc">
+        <div class="text">
+          <div class="b remark">{{ cam.remark ? `${cam.remark}` : cam.meta.manufacturer }}</div>
+        <div class="b model" v-if="cam.meta.manufacturer">{{ cam.meta.manufacturer }}</div>
+        <div class="b model" v-if="cam.meta.model">{{ cam.meta.model }}</div>
+        </div>
+        <div class="enter">Enter</div>
       </div>
     </li>
   </ul>
@@ -108,29 +126,80 @@ ul {
 }
 li {
   display: flex;
+  width: 600px;
   flex-direction: column;
+  position: relative;
 }
-li,
+li,.bg,
 .video {
-  width: 800px;
   color: #fff;
 }
-.video {
+.video, .bg {
+  aspect-ratio: 16/9;
+  width: 600px;
   border-radius: 5px 5px 0 0;
   background-color: #333;
-  height: 450px;
+}
+.bg {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li .desc {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  padding: 10px;
+  background-color: #f6f8fa;
+  color: #023630;
+}
+
+li .desc .text {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+li .desc .b {
+  margin-right: 10px;
+}
+
+li .desc .remark {font-weight: bold;}
+
+li .desc .model {
+  font-size: 12px;
+  color: #0f6157;
+}
+
+li .desc .enter {
+  width: 52px;
+  height: 42px;
+  line-height: 42px;
+  cursor: pointer;
+  color: #023630;
+  border-radius: 5px;
+  text-align: center;
+}
+
+li .desc .enter:hover {
+  color: #0f6157;
 }
 
 li .menu {
   display: flex;
   align-items: center;
-  justify-content:space-between;
+  justify-content: space-between;
   height: 50px;
   padding: 10px;
-  background-color: #f6f8fa;
+  position: absolute;
+  bottom: 50px;
+  z-index: 100;
+  width: 100%;
 }
 li .menu .media {
-  color: #023630;
+  color: #ccc;
 }
 li .menu .media label {
   margin: 5px;
