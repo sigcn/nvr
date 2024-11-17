4c7447f9cf37babd48f19f2b0e62f7a2547bc1c4
rkonfj <rkonfj@gmail.com>
2024-11-17 18:20:43 +0800

    ui/video: add ratelimiter

--- ui/src/components/Video.vue
+++ ui/src/components/Video.vue
@@ -35,7 +35,7 @@ async function init(src, pos) {
   if (mpegts.getFeatureList().mseLivePlayback) {
     let url = `${src}?api_key=${session.key}`
     if (pos) {
-      url = `${url}&pos=${pos}`
+      url = `${url}&pos=${pos}&rate=204800`
       media.value.pos = pos
     }
     player.value = mpegts.createPlayer({
